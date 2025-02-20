238a6e081b86a5939a4c76446b7ff57b206c255c
Jayden Kim <hkim@lcs.on.ca>
2025-02-20 10:00:03 -0500

    Initial Commit

---
+++ MVVMAndFreeforminput/MVVMAndFreeforminputApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMAndFreeforminputApp.swift
+//  MVVMAndFreeforminput
+//
+//  Created by kim heejun on 2/20/25.
+//
+
+import SwiftUI
+
+@main
+struct MVVMAndFreeforminputApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
